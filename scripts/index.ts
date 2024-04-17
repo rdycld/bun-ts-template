cabc132421aaa7613c38959661cb01ee00566c42
Bartosz Ciach <bartoszciach99@gmail.com>
2024-04-17 21:54:13 +0200

    stuff

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,114 +1,69 @@
 /// <reference lib="dom" />
-// /// <reference lib="dom.iterable" />
-//
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-//
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-//
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-//
-// camera.position.z = 5;
-//
-// const raycaster = new THREE.Raycaster();
-// const pointer = new THREE.Vector2(-1, -1);
-//
-// function onPointerMove(event: MouseEvent) {
-//   // calculate pointer position in normalized device coordinates
-//   // (-1 to +1) for both components
-//
-//   pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-// }
-//
-// function intersects() {
-//   raycaster.setFromCamera(pointer, camera);
-//   const intersections = raycaster.intersectObjects(scene.children);
-//
-//   for (const child of scene.children) {
-//     if (raycaster.intersectObject(child, false).length === 0) {
-//       child.material.color.set(0x00ff00);
-//     }
-//   }
-//
-//   for (const obj of intersections) {
-//     obj.object.material.color.set(0xff0000);
-//   }
-// }
-//
-// function click(e: MouseEvent) {
-//   const pointer = new THREE.Vector2(
-//     (e.clientX / window.innerWidth) * 2 - 1,
-//     -(e.clientY / window.innerHeight) * 2 + 1,
-//   );
-//   raycaster.setFromCamera(pointer, camera);
-//
-//   const intersections = raycaster.intersectObjects(scene.children);
-//   for (const i of intersections) {
-//     console.log(i.point);
-//   }
-// }
-//
-// //controls.update() must be called after any manual changes to the camera's transform
-// camera.position.set(0, 20, 100);
-// //
-// //
-// //
-// //
-// //
-// const controls = new OrbitControls(camera, renderer.domElement);
-// controls.update();
-// controls.listenToKeyEvents(window); // optional
-//
-// // controls.addEventListener('change', render); // call this only in static scenes (i.e., if there is no animation loop)
-// //
-// controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-// controls.dampingFactor = 0.05;
-// //
-// controls.screenSpacePanning = false;
-// //
-// controls.minDistance = 100;
-// controls.maxDistance = 500;
-// //
-// controls.maxPolarAngle = Math.PI / 2;
-// function animate() {
-//   requestAnimationFrame(animate);
-//   intersects();
-//
-//   controls.update();
-//   renderer.render(scene, camera);
-// }
-// animate();
-// window.addEventListener('click', click);
-// window.addEventListener('pointermove', onPointerMove);
-//
+/// <reference lib="dom.iterable" />
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const root = document.getElementById('root');
 
-const scene = new THREE.Scene();
+if (!root) throw new Error('xd');
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+const container = document.createElement('div');
 
-const controls = new OrbitControls(camera, renderer.domElement);
+container.style.width = '100%';
+container.style.height = '100%';
+container.style.display = 'flex';
+container.style.justifyContent = 'space-evenly';
+container.style.alignItems = 'flex-end';
 
-//controls.update() must be called after any manual changes to the camera's transform
-camera.position.set(0, 20, 100);
-controls.update();
+class BarChart {
+  #series: number[][];
+  #normalizedSeries: number[][];
+  #maxLength: number;
 
-function animate() {
-  requestAnimationFrame(animate);
+  constructor(series: number[][]) {
+    this.#series = series;
+    this.#normalizedSeries = normalizeSeries(series);
 
-  // required if controls.enableDamping or controls.autoRotate are set to true
-  controls.update();
+    this.#maxLength = series.map((s) => s.length).sort((a, b) => b - a)[0] ?? 0;
+  }
 
-  renderer.render(scene, camera);
+  plot() {
+    for (let i = 0; i < this.#maxLength; ++i) {
+      const entry = document.createElement('div');
+
+      entry.style.display = 'flex';
+      entry.style.height = '100%';
+      entry.style.alignItems = 'flex-end';
+
+      for (let j = 0; j < this.#normalizedSeries.length; ++j) {
+        const val = this.#normalizedSeries[j]?.[i] ?? 0;
+
+        const bar = document.createElement('div');
+
+        bar.style.height = `${val}%`;
+        bar.style.width = '10px';
+
+        const colorBase = (Math.floor(255 / this.#series.length) * j).toString(16);
+        const color = colorBase.padEnd(2, colorBase);
+        bar.style.backgroundColor = `#${color}${color}ff`;
+
+        entry.appendChild(bar);
+      }
+      container.appendChild(entry);
+    }
+  }
+}
+
+const seriesA = [110, 231, 44, 11, 343];
+const seriesB = [11, 131, 444, 211, 33];
+const seriesC = [211, 31, 1, 51, 233];
+
+const chart = new BarChart([seriesA, seriesB, seriesC]);
+
+chart.plot();
+
+root.appendChild(container);
+
+function normalizeSeries(series: number[][]) {
+  const max = Math.max(...series.flatMap((x) => x));
+
+  return series.map((s) => s.map((val) => Number((val / max).toFixed(3)) * 100));
 }
