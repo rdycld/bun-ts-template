0ac242de74161e1d2bedae4524592f18e3080a6b
Bartosz Ciach <bartoszciach99@gmail.com>
2024-04-18 00:31:24 +0200

    more stuff

--- scripts/index.ts
+++ scripts/index.ts
@@ -6,9 +6,10 @@ const root = document.getElementById('root');
 if (!root) throw new Error('xd');
 
 type BarChartOptions = {
-  barWidth: string;
-  barSpace: string;
-  entrySpace: string;
+  barWidth?: string;
+  barSpace?: string;
+  entrySpace?: string;
+  labels?: string[];
 };
 
 class BarChart {
@@ -36,6 +37,52 @@ class BarChart {
     this.#chart.appendChild(this.#container);
   }
 
+  yAxisLabels() {
+    const max = Math.max(...this.#series.flatMap((x) => x));
+    const height = this.#container.getBoundingClientRect().height;
+    const space = height / 50;
+
+    const noLabels = Math.floor(space);
+
+    const labelsContainer = document.createElement('div');
+    labelsContainer.classList.add('y-axis-labels');
+
+    for (let i = 0; i <= noLabels; ++i) {
+      const label = document.createElement('div');
+      label.classList.add('y-axis-label');
+
+      label.innerText = `${Math.floor((noLabels - i) * (max / noLabels))}`;
+
+      console.log(i);
+
+      label.style.top = `${i * (height / noLabels)}px`;
+
+      labelsContainer.appendChild(label);
+    }
+
+    this.#chart.appendChild(labelsContainer);
+  }
+
+  xAxisLabels() {
+    const width = this.#container.getBoundingClientRect().width;
+
+    const labelsContainer = document.createElement('div');
+    labelsContainer.classList.add('x-axis-labels');
+
+    ['jan', 'feb', 'mar', 'apr', 'may'].forEach((mon, idx) => {
+      const label = document.createElement('div');
+      // label.classList.add('x-axis-label');
+      //
+      // label.style.left = `${idx * (width / 5)}px`;
+
+      label.innerText = mon;
+
+      labelsContainer.appendChild(label);
+    });
+
+    this.#chart.appendChild(labelsContainer);
+  }
+
   plot() {
     for (let i = 0; i < this.#maxLength; ++i) {
       const entry = document.createElement('div');
@@ -56,22 +103,26 @@ class BarChart {
         bar.style.width = this.#barWidth;
 
         const colorBase = (Math.floor(255 / this.#series.length) * j).toString(16);
-        const color = colorBase.padEnd(2, colorBase);
+        const color = colorBase.padStart(2, '0');
         bar.style.backgroundColor = `#${color}${color}ff`;
 
         entry.appendChild(bar);
       }
       this.#container.appendChild(entry);
     }
+
     root?.appendChild(this.#chart);
+    this.yAxisLabels();
+    this.xAxisLabels();
   }
 }
 
 const seriesA = [110, 231, 44, 11, 343];
 const seriesB = [11, 131, 444, 211, 33];
-const seriesC = [211, 31, 1, 51, 233];
+const seriesC = [50, 11, 20, 51, 233];
+const seriesD = [150, 301, 130, 271, 133];
 
-const chart = new BarChart([seriesA, seriesB, seriesC]);
+const chart = new BarChart([seriesA, seriesB, seriesC, seriesD]);
 
 chart.plot();
 
